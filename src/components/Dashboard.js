d413ca91ead1221a2ab097b356e3005fcf5d11fa
togmund <togmund@gmail.com>
2019-12-11 20:37:05 -0800

    added a new Panel component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,9 +2,32 @@ import React, { Component } from "react";
 
 import classnames from "classnames";
 
+const data = [
+  {
+    id: 1,
+    label: "Total Interviews",
+    value: 6
+  },
+  {
+    id: 2,
+    label: "Least Popular Time Slot",
+    value: "1pm"
+  },
+  {
+    id: 3,
+    label: "Most Popular Day",
+    value: "Wednesday"
+  },
+  {
+    id: 4,
+    label: "Interviews Per Day",
+    value: "2.3"
+  }
+];
+
 class Dashboard extends Component {
   state = {
-    loading: true
+    loading: false
   };
 
   render() {
